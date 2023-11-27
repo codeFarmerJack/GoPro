1defd20da12f95e633716623b91bb71b48c6f12f
codeFarmerJack <116141443+codeFarmerJack@users.noreply.github.com>
2023-11-27 13:15:06 +0800

    possible to plot a comparison between the signal before and after filtering

--- GoPro_Opti_v02.py
+++ GoPro_Opti_v02.py
@@ -5,6 +5,7 @@ from datetime import datetime as dt
 from datetime import timedelta
 from scipy.signal import butter, filtfilt
 import matplotlib.pyplot as plt
+import mplcursors
 
 def butter_lowpass_filter(data, cutoff_freq, sample_rate, order=4):
     nyquist = 0.5 * sample_rate     
@@ -23,42 +24,61 @@ def dataOpt(rawData, speed, accLong, accLat, jerk):
     cutoff_freq = 0.07      # Adjust this cutoff frequency as needed
 
     # Filter the raw data 
-    VspeedOpt = butter_lowpass_filter(rawData[speed], cutoff_freq, SampleRate, order=3)
-    acclLongOptFilt = butter_lowpass_filter(rawData[accLong], cutoff_freq, SampleRate, order=3)
-    acclLatOptFilt = butter_lowpass_filter(rawData[accLat], cutoff_freq, SampleRate, order=3)
+    speedOpt = butter_lowpass_filter(rawData[speed], cutoff_freq, SampleRate, order=3)
+    accLongOptFilt = butter_lowpass_filter(rawData[accLong], cutoff_freq, SampleRate, order=3)
+    accLatOptFilt = butter_lowpass_filter(rawData[accLat], cutoff_freq, SampleRate, order=3)
     jerkOptFilt = butter_lowpass_filter(rawData[jerk], cutoff_freq, SampleRate, order=3)
-    VspeedOptKPH = VspeedOpt * 3.6
+    speedOptKPH = speedOpt * 3.6
 
     # insert to the original DataFrame
-    rawData.insert(loc=len(rawData.columns), column = 'VspeedOpt', value=VspeedOpt)
-    rawData.insert(loc=len(rawData.columns), column = 'acclLongOptFilt', value=acclLongOptFilt)
-    rawData.insert(loc=len(rawData.columns), column = 'acclLatOptFilt', value=acclLatOptFilt)
+    rawData.insert(loc=len(rawData.columns), column = 'speedOpt', value=speedOpt)
+    rawData.insert(loc=len(rawData.columns), column = 'accLongOptFilt', value=accLongOptFilt)
+    rawData.insert(loc=len(rawData.columns), column = 'accLatOptFilt', value=accLatOptFilt)
     rawData.insert(loc=len(rawData.columns), column = 'jerkOptFilt', value=jerkOptFilt)
-    rawData.insert(loc=len(rawData.columns), column = 'VspeedOptKPH', value=VspeedOptKPH)
+    rawData.insert(loc=len(rawData.columns), column = 'speedOptKPH', value=speedOptKPH)
     
     return rawData 
 
 def custom_date_parser(x):
     return pd.to_datetime(x, format = '%H:%M:%S', errors='coerce')
 
+"""
 def plot_dynamic_subplot(x, *y_data):
-    num_plots = len(y_data)
+    if len(y_data) % 2 != 0:
+        raise ValueError('Number of y_data arguments should be even.')
+    
+    num_plots = len(y_data) // 2
 
-    fig, axes = plt.subplots(num_plots, 1, figsize = (15, 2 * num_plots))
+    fig, axes = plt.subplots(num_plots, 1, sharex = True, figsize = (15, 2 * num_plots))
 
     if num_plots == 1:
         axes = [axes]   # Ensure axes is a list even for a single plot
 
-    for i, (y_value, y_name) in enumerate(y_data):
-        axes[i].plot(x, y_value, label = y_name)
+    # Plot data on each subplot
+    for i in range(0, len(y_data), 2):
+        if i % 2 == 0:
+
+        
+
+    # Set common X-axis label
+    # axes[-1].set_xlabel('X-axis')
+    
+    for i, y_value in enumerate(y_data):
+        axes[i].plot(x, y_value)
         axes[i].set_xlabel('X-axis')
-        axes[i].set_ylabel(f'Y-axis {y_name}')
+        axes[i].set_ylabel(f'Y-axis {i + 1}')
         axes[i].legend()
+    
     
     # Adjust layout to prevent clipping of titles
     plt.tight_layout()
 
+    # Add labels to data points 
+    mplcursors.cursor(axes).connect('add', 
+                                    lambda sel: sel.annotation.set_text(f'X: ({sel.target[0]:.2f}, Y: {sel.target[1]:.2f})'),
+                                    )
     plt.show()
+"""
 
 if __name__ == '__main__':
 
@@ -100,44 +120,64 @@ if __name__ == '__main__':
     optimizedGopro = dataOpt(gopro, speedGopro, accLongGopro, accLatGopro, jerkGopro)
     optimizedRT = dataOpt(rt, speedRT, accLongRT, accLatRT, jerkRT)
 
-    plot_dynamic_subplot(optimizedRT['date_01'], optimizedRT[accLatRT], optimizedRT['acclLatOptFilt'])
+    # plot_dynamic_subplot(optimizedRT['date_01'], optimizedRT[accLatRT], optimizedRT['acclLatOptFilt'])
+    
 
-
-    """plt.figure(1)
     plt.figure(figsize=(15, 20))
 
-    plt.subplot(3,1,1)
-    plt.plot(optimizedGopro["date_01"], optimizedGopro[accLatGopro], label="acclLateral")
-    plt.plot(optimizedGopro["date_01"], optimizedGopro['acclLatOptFilt'], label="acclLatOptFilt")
+    plt.subplot(4,2,1)
+    plt.plot(optimizedGopro["date_01"], optimizedGopro[speedGopro], label="speedGopro")
+    plt.plot(optimizedGopro["date_01"], optimizedGopro['speedOpt'], label="speedOpt")
     plt.legend(fontsize=5)
     plt.legend(loc='upper left')
 
-    plt.subplot(3,1,2)
-    plt.plot(rt["date_01"], rt[accLatRT], label="accelerationLateralRT")
-    plt.plot(optimizedRT["date_01"], optimizedRT['acclLatOptFilt'], label="acclLatOptFiltRT")
+    plt.subplot(4,2,3)
+    plt.plot(optimizedGopro["date_01"], optimizedGopro[accLongGopro], label="accLongGopro")
+    plt.plot(optimizedGopro["date_01"], optimizedGopro['accLongOptFilt'], label="accLongOptFilt")
     plt.legend(fontsize=5)
     plt.legend(loc='upper left')
 
-    plt.subplot(3,1,3)
-    plt.plot(optimizedGopro["date_01"], optimizedGopro['acclLatOptFilt'], label="acclLatOptFilt")
-    plt.plot(optimizedRT["date_01"], optimizedRT['acclLatOptFilt'], label="acclLatOptFiltRT")
+    plt.subplot(4,2,5)
+    plt.plot(optimizedGopro["date_01"], optimizedGopro[accLatGopro], label="accLatGopro")
+    plt.plot(optimizedGopro["date_01"], optimizedGopro['accLatOptFilt'], label="accLatOptFilt")
     plt.legend(fontsize=5)
     plt.legend(loc='upper left')
 
-    plt.show()"""
+    plt.subplot(4,2,7)
+    plt.plot(optimizedGopro["date_01"], optimizedGopro[jerkGopro], label="jerkGopro")
+    plt.plot(optimizedGopro["date_01"], optimizedGopro['jerkOptFilt'], label="jerkOptFilt")
+    plt.legend(fontsize=5)
+    plt.legend(loc='upper left')
+
+    plt.subplot(4,2,2)
+    plt.plot(optimizedRT["date_01"], optimizedRT[speedRT], label="speedRT")
+    plt.plot(optimizedRT["date_01"], optimizedRT['speedOpt'], label="speedOpt")
+    plt.legend(fontsize=5)
+    plt.legend(loc='upper left')
+
+    plt.subplot(4,2,4)
+    plt.plot(optimizedRT["date_01"], optimizedRT[accLongRT], label="accLongRT")
+    plt.plot(optimizedRT["date_01"], optimizedRT['accLongOptFilt'], label="accLongOptFilt")
+    plt.legend(fontsize=5)
+    plt.legend(loc='upper left')
+
+    plt.subplot(4,2,6)
+    plt.plot(optimizedRT["date_01"], optimizedRT[accLatRT], label="accLatRT")
+    plt.plot(optimizedRT["date_01"], optimizedRT['accLatOptFilt'], label="accLatOptFiltRT")
+    plt.legend(fontsize=5)
+    plt.legend(loc='upper left')
+
+    plt.subplot(4,2,8)
+    plt.plot(optimizedRT["date_01"], optimizedRT[jerkRT], label="jerkRT")
+    plt.plot(optimizedRT["date_01"], optimizedRT['jerkOptFilt'], label="jerkOptFilt")
+    plt.legend(fontsize=5)
+    plt.legend(loc='upper left')
+
+
+    plt.tight_layout()
+    # Add labels to data points 
+    """mplcursors.cursor(axes).connect('add', 
+                                    lambda sel: sel.annotation.set_text(f'X: ({sel.target[0]:.2f}, Y: {sel.target[1]:.2f})'),
+                                    )"""
     
-    # Base comparison
-    if gopro is not None and rt is not None:
-        mergedData = pd.merge(gopro, rt, on="date_01", how='inner')
-        # plot the base comparision
-        
-        # plotFigure(mergedData, VspeedGopro, accelerationLongiGopro, accelerationLateralGopro, jerkGopro)
-        # plt.show()
-
-        # plot the optimized comparision
-        # mergedDataOpt = pd.merge(optimized, rt, on='date_01', how='inner')
-        # print(mergedDataOpt.columns)
-        # plotFigure(mergedDataOpt, mergedDataOpt['VspeedOpt'], mergedDataOpt['acclLongOptFilt'],mergedDataOpt['acclLatOptFilt'],
-        #            mergedDataOpt['jerkOptFilt'])
-        # plt.show()
-    
+    plt.show()
