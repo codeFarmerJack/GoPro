4aa6f6419831d659fccd9c2d14a710e1c239e6ab
codeFarmerJack <116141443+codeFarmerJack@users.noreply.github.com>
2023-12-03 16:44:37 +0800

    cursor can move as expected - before annotation

--- GoPro_Opti_v02.py
+++ GoPro_Opti_v02.py
@@ -49,34 +49,10 @@ def draw_cursor(ax, x_values):
         
 # Callback function for mouse click event
 def on_plot_click(event):
-    global initial_x_value
+    global target_x_value
     if event.inaxes is not None and event.button == 1:
-        initial_x_value = event.xdata
-
-        # Iterate through the subplot layout
-        for df, date_column, data, label, filtered_data in subplot_layout:
-            
-            """if initial_x_value in df[date_column].values:
-                y_value_data = df[data][df[date_column] == initial_x_value].values[0]
-                y_value_filtered = df[filtered_data][df[date_column] == initial_x_value].values[0]
-            else:
-                # If initial_x_value is not in the DataFrame, perform interpolation
-                y_value_data = np.interp(initial_x_value, df[date_column], df[data])
-                y_value_filtered = np.interp(initial_x_value, df[date_column], df[filtered_data])
-            """
-            # print(y_value_data)
-            # print(y_value_filtered)
-            
-            # Update text annotation at the bottom right of each subplot
-            # subplot_index = i - 1
-            # ax = plt.subplot(4, 2, i)
-            # text_annotation = f'{label}: {y_value_data:.2f} \n{filtered_data}Opt: {y_value_filtered:.2f}'
-
-            # Remove previous annotations
-            # for annotation in ax.texts:
-            #     annotation.remove()
-            # Add new annotation
-            # ax.annotate(text_annotation, xy=(1, 0), xycoords='axes fraction', ha = 'right', va = 'bottom')
+        
+        target_x_value = event.xdata
 
         for ax in plt.gcf().get_axes():
             # Remove the existing cursor with "cursor" label
@@ -84,16 +60,8 @@ def on_plot_click(event):
             for line in existing_lines:
                 line.remove()
             # Draw a new cursor at the updated x-value
-            draw_cursor(ax, x_values=[initial_x_value])
+            draw_cursor(ax, x_values=[target_x_value])
             
-            if initial_x_value in df[date_column].values:
-                y_value_data = df[data][df[date_column] == initial_x_value].values[0]
-                y_value_filtered = df[filtered_data][df[date_column] == initial_x_value].values[0]
-            else:
-                # If initial_x_value is not in the DataFrame, perform interpolation
-                y_value_data = np.interp(initial_x_value, df[date_column], df[data])
-                y_value_filtered = np.interp(initial_x_value, df[date_column], df[filtered_data])
-
         plt.gcf().canvas.draw()
 
 if __name__ == '__main__':
@@ -138,44 +106,44 @@ if __name__ == '__main__':
 
     # plot_dynamic_subplot(optimizedRT['date_01'], optimizedRT[accLatRT], optimizedRT['acclLatOptFilt'])
     
-initial_x_value = None
+    target_x_value = None
 
-# Determine the common date range
-common_date_range = pd.to_datetime(np.intersect1d(optimizedGopro["date_01"], optimizedRT["date_01"]))
+    # Determine the common date range
+    common_date_range = pd.to_datetime(np.intersect1d(optimizedGopro["date_01"], optimizedRT["date_01"]))
 
-plt.figure(figsize=(15, 10))
+    plt.figure(figsize=(15, 10))
 
-# Define the subplot layout
-subplot_layout = [
-    (optimizedGopro, 'date_01', optimizedGopro[speedGopro], 'speedGopro', 'speedOpt'),
-    (optimizedRT, 'date_01', optimizedRT[speedRT], 'speedRT', 'speedOpt'),
-    (optimizedGopro, 'date_01', optimizedGopro[accLongGopro], 'accLongGopro', 'accLongOptFilt'),
-    (optimizedRT, 'date_01', optimizedRT[accLongRT], 'accLongRT', 'accLongOptFilt'),
-    (optimizedGopro, 'date_01', optimizedGopro[accLatGopro], 'accLatGopro', 'accLatOptFilt'),
-    (optimizedRT, 'date_01', optimizedRT[accLatRT], 'accLatRT', 'accLatOptFilt'),
-    (optimizedGopro, 'date_01', optimizedGopro[jerkGopro], 'jerkGopro', 'jerkOptFilt'),
-    (optimizedRT, 'date_01', optimizedRT[jerkRT], 'jerkRT', 'jerkOptFilt')
-]
+    # Define the subplot layout 
+    subplot_layout = [
+        (optimizedGopro, 'date_01', optimizedGopro[speedGopro], 'speedGopro', 'speedOpt'),
+        (optimizedRT, 'date_01', optimizedRT[speedRT], 'speedRT', 'speedOpt'),
+        (optimizedGopro, 'date_01', optimizedGopro[accLongGopro], 'accLongGopro', 'accLongOptFilt'),
+        (optimizedRT, 'date_01', optimizedRT[accLongRT], 'accLongRT', 'accLongOptFilt'),
+        (optimizedGopro, 'date_01', optimizedGopro[accLatGopro], 'accLatGopro', 'accLatOptFilt'),
+        (optimizedRT, 'date_01', optimizedRT[accLatRT], 'accLatRT', 'accLatOptFilt'),
+        (optimizedGopro, 'date_01', optimizedGopro[jerkGopro], 'jerkGopro', 'jerkOptFilt'),
+        (optimizedRT, 'date_01', optimizedRT[jerkRT], 'jerkRT', 'jerkOptFilt')
+    ]
 
-# Connect the mouse click event to the callback function
-plt.gcf().canvas.mpl_connect('button_press_event', on_plot_click)
-# mplcursors.cursor(hover = True).connect('add', on_plot_click)
+    # Connect the mouse click event to the callback function
+    plt.gcf().canvas.mpl_connect('button_press_event', on_plot_click)
+    # mplcursors.cursor(hover = True).connect('add', on_plot_click)
 
-# Iterate through the subplot layout
-for i, (df, date_column, data, label, filtered_data) in enumerate(subplot_layout, start=1):
-    ax = plt.subplot(4, 2, i)
+    # Iterate through the subplot layout
+    for i, (df, date_column, data, label, filtered_data) in enumerate(subplot_layout, start=1):
+        ax = plt.subplot(4, 2, i)
 
-    # Select the common date range for each subplot
-    common_data = data[df[date_column].isin(common_date_range)]
-    common_filtered_data = df[filtered_data][df[date_column].isin(common_date_range)]
+        # Select the common date range for each subplot
+        common_data = data[df[date_column].isin(common_date_range)]
+        common_filtered_data = df[filtered_data][df[date_column].isin(common_date_range)]
     
-    # Plot data and filtered data
-    ax.plot(common_date_range, common_data, label=label)
-    ax.plot(common_date_range, common_filtered_data, label=f'{filtered_data}Opt')
+        # Plot data and filtered data
+        ax.plot(common_date_range, common_data, label=label)
+        ax.plot(common_date_range, common_filtered_data, label=f'{filtered_data}Opt')
     
-    draw_cursor(ax, x_values = [common_date_range[0]])
+        draw_cursor(ax, x_values = [common_date_range[0]])
     
-    ax.legend(fontsize = 5, loc = 'upper left')
+        ax.legend(fontsize = 5, loc = 'upper left')
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
